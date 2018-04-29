1f592d22c78f0f9bbe1ac69dd449c13eb4ccb846
jye <jye@coupang.com>
2018-04-29 14:21:55 +0900

    Initial Commit

---
+++ Focus25/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Focus25
+//
+//  Created by Jun Ye on 4/29/18.
+//  Copyright © 2018 Jun Ye. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
